75793974bf3330b132bc20cd4b939e3ec8679f26
Naresh Singh <nareshsingh0170@gmail.com>
2024-10-12 21:15:07 +0530

    Version 1 Complete

--- js/script.js
+++ js/script.js
@@ -56,27 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
     typeSpeed: 100,
     backSpeed: 80,
     backDelay: 1000,
-  });
-
-
-  /*==================== PORTFOLIO SWIPER  ====================*/
-  let swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    loop: true,
-
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
-
-  // FORM BORDERS 
+  }); 
 
 });
 
